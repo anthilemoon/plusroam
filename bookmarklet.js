2cd87f08c31c50df15f50eb4e2a2810f114c65a1
Joshua Pham <jpham@spotify.com>
2020-04-30 19:48:03 -0400

    Paste multiline text into a single bullet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,2 @@
-(function(){let text=""; if(window.getSelection()!=''){text=window.getSelection().toString();}prompt("Press CTRL+C or CMD+C, then escape and paste into Roam.", "__"+text+"__ — via ["+document.title+"]("+location.href+") [[+Roam]]");})()
+(function(){let text="";if(window.getSelection()!=''){text=window.getSelection().toString().replace(/(\r\n|\n|\r)/gm,"\r")}
+let formatted="__"+text.split("\r").join("__\r__")+"__";prompt("Press CTRL+C or CMD+C, then escape and paste into Roam.",formatted+" — via ["+document.title+"]("+location.href+") [[+Roam]]")})()
